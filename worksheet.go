244bacf3cb32f02831215554f8d301fb169772f3
Lucas Barros <lucas.barros@getground.co.uk>
2024-05-02 12:17:30 +0100

    Fix null pointer

--- worksheet.go
+++ worksheet.go
@@ -14,20 +14,22 @@ type boundsheet struct {
 	Name    byte
 }
 
-//WorkSheet in one WorkBook
+// WorkSheet in one WorkBook
 type WorkSheet struct {
 	bs   *boundsheet
 	wb   *WorkBook
 	Name string
 	rows map[uint16]*Row
-	//NOTICE: this is the max row number of the sheet, so it should be count -1
+	// NOTICE: this is the max row number of the sheet, so it should be count -1
 	MaxRow uint16
 	parsed bool
 }
 
 func (w *WorkSheet) Row(i int) *Row {
 	row := w.rows[uint16(i)]
-	row.wb = w.wb
+	if row != nil {
+		row.wb = w.wb
+	}
 	return row
 }
 
@@ -54,11 +56,11 @@ func (w *WorkSheet) parseBof(buf io.ReadSeeker, b *bof, pre *bof) *bof {
 	switch b.Id {
 	// case 0x0E5: //MERGEDCELLS
 	// ws.mergedCells(buf)
-	case 0x208: //ROW
+	case 0x208: // ROW
 		r := new(rowInfo)
 		binary.Read(buf, binary.LittleEndian, r)
 		w.addRow(r)
-	case 0x0BD: //MULRK
+	case 0x0BD: // MULRK
 		mc := new(MulrkCol)
 		size := (b.Size - 6) / 6
 		binary.Read(buf, binary.LittleEndian, &mc.Col)
@@ -68,7 +70,7 @@ func (w *WorkSheet) parseBof(buf io.ReadSeeker, b *bof, pre *bof) *bof {
 		}
 		binary.Read(buf, binary.LittleEndian, &mc.LastColB)
 		col = mc
-	case 0x0BE: //MULBLANK
+	case 0x0BE: // MULBLANK
 		mc := new(MulBlankCol)
 		size := (b.Size - 6) / 2
 		binary.Read(buf, binary.LittleEndian, &mc.Col)
@@ -78,19 +80,19 @@ func (w *WorkSheet) parseBof(buf io.ReadSeeker, b *bof, pre *bof) *bof {
 		}
 		binary.Read(buf, binary.LittleEndian, &mc.LastColB)
 		col = mc
-	case 0x203: //NUMBER
+	case 0x203: // NUMBER
 		col = new(NumberCol)
 		binary.Read(buf, binary.LittleEndian, col)
-	case 0x06: //FORMULA
+	case 0x06: // FORMULA
 		c := new(FormulaCol)
 		binary.Read(buf, binary.LittleEndian, &c.Header)
 		c.Bts = make([]byte, b.Size-20)
 		binary.Read(buf, binary.LittleEndian, &c.Bts)
 		col = c
-	case 0x27e: //RK
+	case 0x27e: // RK
 		col = new(RkCol)
 		binary.Read(buf, binary.LittleEndian, col)
-	case 0xFD: //LABELSST
+	case 0xFD: // LABELSST
 		col = new(LabelsstCol)
 		binary.Read(buf, binary.LittleEndian, col)
 	case 0x204:
@@ -100,10 +102,10 @@ func (w *WorkSheet) parseBof(buf io.ReadSeeker, b *bof, pre *bof) *bof {
 		binary.Read(buf, binary.LittleEndian, &count)
 		c.Str, _ = w.wb.get_string(buf, count)
 		col = c
-	case 0x201: //BLANK
+	case 0x201: // BLANK
 		col = new(BlankCol)
 		binary.Read(buf, binary.LittleEndian, col)
-	case 0x1b8: //HYPERLINK
+	case 0x1b8: // HYPERLINK
 		var hy HyperLink
 		binary.Read(buf, binary.LittleEndian, &hy.CellRange)
 		buf.Seek(20, 1)
@@ -122,11 +124,11 @@ func (w *WorkSheet) parseBof(buf io.ReadSeeker, b *bof, pre *bof) *bof {
 		if flag&0x1 != 0 {
 			var guid [2]uint64
 			binary.Read(buf, binary.BigEndian, &guid)
-			if guid[0] == 0xE0C9EA79F9BACE11 && guid[1] == 0x8C8200AA004BA90B { //URL
+			if guid[0] == 0xE0C9EA79F9BACE11 && guid[1] == 0x8C8200AA004BA90B { // URL
 				hy.IsUrl = true
 				binary.Read(buf, binary.LittleEndian, &count)
 				hy.Url = b.utf16String(buf, count/2)
-			} else if guid[0] == 0x303000000000000 && guid[1] == 0xC000000000000046 { //URL{
+			} else if guid[0] == 0x303000000000000 && guid[1] == 0xC000000000000046 { // URL{
 				var upCount uint16
 				binary.Read(buf, binary.LittleEndian, &upCount)
 				binary.Read(buf, binary.LittleEndian, &count)
